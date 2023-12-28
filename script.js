0748e84c6779f98603bff004c4303e6d58aa403f
sspccreates <145820126+sspccreates@users.noreply.github.com>
2023-12-27 23:06:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,39 +6,48 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("events.txt")
         .then(response => response.text())
         .then(data => {
+            console.log("Raw data:", data); // Log raw data for debugging
+
             // Process the data and display events
             const eventsData = data.split("\n");
             eventsData.forEach(eventData => {
+                console.log("Event data:", eventData); // Log each event data for debugging
+
                 const [title, date, location, link, description] = eventData.split("|");
 
-                // Create a container for each event
-                const eventContainer = document.createElement("div");
-                eventContainer.classList.add("event-box");
+                // Check if all fields are defined
+                if (title !== undefined && date !== undefined && location !== undefined && link !== undefined && description !== undefined) {
+                    // Create a container for each event
+                    const eventContainer = document.createElement("div");
+                    eventContainer.classList.add("event-box");
 
-                // Event title
-                const titleElement = document.createElement("h2");
-                titleElement.textContent = title;
-                eventContainer.appendChild(titleElement);
+                    // Event title
+                    const titleElement = document.createElement("h2");
+                    titleElement.textContent = title;
+                    eventContainer.appendChild(titleElement);
 
-                // Event date and location
-                const infoElement = document.createElement("p");
-                infoElement.textContent = `${date} | ${location}`;
-                eventContainer.appendChild(infoElement);
+                    // Event date and location
+                    const infoElement = document.createElement("p");
+                    infoElement.textContent = `${date} | ${location}`;
+                    eventContainer.appendChild(infoElement);
 
-                // Event description
-                const descriptionElement = document.createElement("p");
-                descriptionElement.textContent = description;
-                eventContainer.appendChild(descriptionElement);
+                    // Event description
+                    const descriptionElement = document.createElement("p");
+                    descriptionElement.textContent = description;
+                    eventContainer.appendChild(descriptionElement);
 
-                // Learn more link
-                const linkElement = document.createElement("a");
-                linkElement.textContent = "Learn more";
-                linkElement.href = link;
-                linkElement.target = "_blank"; // Open link in a new tab
-                eventContainer.appendChild(linkElement);
+                    // Learn more link
+                    const linkElement = document.createElement("a");
+                    linkElement.textContent = "Learn more";
+                    linkElement.href = link;
+                    linkElement.target = "_blank"; // Open link in a new tab
+                    eventContainer.appendChild(linkElement);
 
-                // Append the event container to the main container
-                eventsContainer.appendChild(eventContainer);
+                    // Append the event container to the main container
+                    eventsContainer.appendChild(eventContainer);
+                } else {
+                    console.error("Invalid event data:", eventData); // Log an error for debugging
+                }
             });
         })
         .catch(error => console.error("Error fetching events:", error));
